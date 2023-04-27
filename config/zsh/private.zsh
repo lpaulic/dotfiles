e637fc85439071af52b921105115de219458f6d8
Luka Paulić <luka.paulic0@gmail.com>
2023-04-27 21:36:01 +0200

    feat: update dot files for olive

--- config/zsh/private.zsh
+++ config/zsh/private.zsh
@@ -1,3 +1,4 @@
 source /usr/share/fzf/key-bindings.zsh
 source /usr/share/fzf/completion.zsh
 export FZF_DEFAULT_OPTS="--height=40% --layout=reverse --border --history=$HISTFILE"
+source "$HOME/.cargo/env"
