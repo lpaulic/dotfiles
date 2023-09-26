6151537dd3d43de260930fcd2616cab66528668a
Luka Paulić <luka.paulic0@gmail.com>
2023-09-26 23:51:13 +0200

    feat: remove vim, fix xdg environment in zsh

--- config/zsh/work_environment.zsh
+++ config/zsh/work_environment.zsh
@@ -1,7 +1,8 @@
 #set XDG path
 export XDG_CONFIG_HOME="$HOME/.config"
-export XDG_DATA_HOME="$HOME/.config"
+export XDG_DATA_HOME="$HOME/.local/share"
 export XDG_CACHE_HOME="$HOME/.cache"
+export XDG_STATE_HOME="$HOME/.local/state"
 
 #set EDITOR
 if [[ -a "/usr/bin/vim" ]]; then
