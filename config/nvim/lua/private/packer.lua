25648c021b070d2e01453691cf59702617437e2a
Luka Paulić <luka.paulic0@gmail.com>
2023-09-27 06:43:49 +0200

    feat: update nvim config with telescope plugin

--- config/nvim/lua/private/packer.lua
+++ config/nvim/lua/private/packer.lua
@@ -7,5 +7,11 @@ return require('packer').startup(function(use)
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
 
+  use {
+	  'nvim-telescope/telescope.nvim', tag = '0.1.3',
+	  -- or                            , branch = '0.1.x',
+	  requires = { {'nvim-lua/plenary.nvim'} }
+  }
+
 end)
 
