f09d0ba817f5047df0da13752daf507182db1056
Luka Paulić <luka.paulic0@gmail.com>
2024-02-11 14:35:15 +0100

    fix: update zsh prompt, and nvim keymaps and options

--- config/nvim/lua/user/package-manager/plugins/completions.lua
+++ config/nvim/lua/user/package-manager/plugins/completions.lua
@@ -1,19 +1,26 @@
 return {
 	{
-        "hrsh7th/cmp-nvim-lsp"
-    },
-    {
+		"hrsh7th/cmp-nvim-lsp",
+	},
+	{
+		"hrsh7th/cmp-path",
+	},
+	{
+		"hrsh7th/cmp-buffer",
+	},
+	{
 		"L3MON4D3/LuaSnip",
 		dependencies = {
 			"saadparwaiz1/cmp_luasnip",
-            "rafamadriz/friendly-snippets",
+			"rafamadriz/friendly-snippets",
 		},
 	},
 	{
 		"hrsh7th/nvim-cmp",
 		config = function()
 			local cmp = require("cmp")
-            require("luasnip.loaders.from_vscode").lazy_load()
+			local select_opts = { behavior = cmp.SelectBehavior.Insert }
+			require("luasnip.loaders.from_vscode").lazy_load()
 
 			cmp.setup({
 				snippet = {
@@ -25,19 +32,37 @@ return {
 					completion = cmp.config.window.bordered(),
 					documentation = cmp.config.window.bordered(),
 				},
-                mapping = cmp.mapping.preset.insert({
-					["<C-b>"] = cmp.mapping.scroll_docs(-4),
-					["<C-f>"] = cmp.mapping.scroll_docs(4),
-					["<C-Space>"] = cmp.mapping.complete(),
+				mapping = {
+					--    ['<Up>'] = cmp.mapping.select_prev_item(select_opts),
+					--    ['<Down>'] = cmp.mapping.select_next_item(select_opts),
+					["<C-p>"] = cmp.mapping.select_prev_item(select_opts),
+					["<C-n>"] = cmp.mapping.select_next_item(select_opts),
+					["<C-u>"] = cmp.mapping.scroll_docs(-4),
+					["<C-d>"] = cmp.mapping.scroll_docs(4),
 					["<C-e>"] = cmp.mapping.abort(),
-					["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-				}),
-				sources = cmp.config.sources({
-					{ name = 'nvim_lsp' },
-					{ name = "luasnip" }, -- For luasnip users.
-				}, {
-					{ name = "buffer" },
-				}),
+					["<C-y>"] = cmp.mapping.confirm({ select = true }),
+					["<CR>"] = cmp.mapping.confirm({ select = false }),
+				},
+				formatting = {
+					fields = { "menu", "abbr", "kind" },
+					format = function(entry, item)
+						local menu_icon = {
+							nvim_lsp = "λ",
+							luasnip = "⋗",
+							buffer = "Ω",
+							path = "🖫",
+						}
+
+						item.menu = menu_icon[entry.source.name]
+						return item
+					end,
+				},
+				sources = {
+					{ name = "path" },
+					{ name = "nvim_lsp", keyword_length = 1 },
+					{ name = "buffer", keyword_length = 3 },
+					{ name = "luasnip", keyword_length = 2 },
+				},
 			})
 		end,
 	},
