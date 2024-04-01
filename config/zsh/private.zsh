27383000ffad28dc804d85652fe6ea8399e7758f
Luka Paulić <luka.paulic@byte-lab.com>
2024-04-01 08:53:52 +0200

    Add oak work laptop config tracking

--- config/zsh/private.zsh
+++ config/zsh/private.zsh
@@ -1,4 +1,7 @@
 source /usr/share/fzf/key-bindings.zsh
 source /usr/share/fzf/completion.zsh
 export FZF_DEFAULT_OPTS="--height=40% --layout=reverse --border --history=$HISTFILE"
-source "$HOME/.cargo/env"
+if [ -e "$HOME/.cargo/env" ]; then
+    source "$HOME/.cargo/env"
+fi
+export WINIT_X11_SCALE_FACTOR=1 # alacrytty DP scaling disable
