53a1bd3d349f72c09270a24b6717f016bdf2d0a1
Luka Paulic <programmer.luka@tuta.com>
2025-06-14 13:03:32 +0400

    Add sourcing of local ruby installation

--- config/zsh/private.zsh
+++ config/zsh/private.zsh
@@ -3,3 +3,7 @@ test -e /usr/share/fzf/completion.zsh && source /usr/share/fzf/completion.zsh
 export FZF_DEFAULT_OPTS="--height=40% --layout=reverse --border --history=$HISTFILE"
 
 test -e "$HOME/.cargo/env" && source "$HOME"/.cargo/env
+
+test -e "$XDG_DATA_HOME/gem/ruby/3.4.0/bin" && export PATH="$XDG_DATA_HOME/gem/ruby/3.4.0/bin:$PATH"
+test -e "$XDG_DATA_HOME/gem/ruby/3.4.0/bin" && export BUNDLE_PATH="$HOME/.gems"
+
